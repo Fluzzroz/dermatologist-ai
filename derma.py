84ca27b604bcaf5086cd13bd5b93b45a3a18874c
Benoît Boucher <boucher.benoit@gmail.com>
2018-03-15 10:44:15 -0400

    plotting a test image and its transformed into 299x299

--- derma.py
+++ derma.py
@@ -1,3 +1,9 @@
-import numpy as np
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
 
-a = np.arange(0,11,1)
+fig = plt.figure()
+original = image.load_img("data/ISIC_0015284.jpg")
+img = image.load_img("data/ISIC_0015284.jpg", target_size=(299,299))
+fig.add_subplot(1,2,1).imshow(original)
+fig.add_subplot(1,2,2).imshow(img)
+plt.show()
